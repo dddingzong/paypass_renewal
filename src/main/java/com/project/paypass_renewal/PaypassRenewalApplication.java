121142bdfced7f2f57f19f6158a148ecfa7088b4
USER <cji513@naver.com>
2025-05-28 20:20:52 +0900

    first commit

---
+++ src/main/java/com/project/paypass_renewal/PaypassRenewalApplication.java
@@ -0,0 +1,13 @@
+package com.project.paypass_renewal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypassRenewalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypassRenewalApplication.class, args);
+	}
+
+}
