73af2d8f0d7dd1198de166f7111b9d96affd7d17
USER <cji513@naver.com>
2025-05-31 17:06:18 +0900

    feat(User): User 도메인 구현

--- src/main/java/com/project/paypass_renewal/domain/User.java
+++ src/main/java/com/project/paypass_renewal/domain/User.java
@@ -1,9 +1,8 @@
 package com.project.paypass_renewal.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class User {
@@ -11,7 +10,26 @@ public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String mainId;
 
+    @Column(nullable = false)
+    private String name;
 
+    @Column(nullable = false)
+    private LocalDateTime birth;
 
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String homeAddress;
+
+    private String centerAddress;
+
+    @Column(nullable = false)
+    private String linkCode;
+
+    @Column(nullable = false)
+    ServiceCode serviceCode;
 }
