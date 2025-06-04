22cbf40d3c05855becc9dd506256d7213ed621a2
USER <cji513@naver.com>
2025-06-04 17:33:33 +0900

    feat(UserUtils): createDummyUse 메서드 작성

---
+++ src/test/java/com/project/paypass_renewal/support/UserUtils.java
@@ -0,0 +1,20 @@
+package com.project.paypass_renewal.support;
+
+import com.project.paypass_renewal.domain.ServiceCode;
+import com.project.paypass_renewal.domain.User;
+
+import java.time.LocalDate;
+
+public class UserUtils {
+
+    public static User createDummyUser() {
+        return new User("dummy@gmail.com",
+                "더미유저",
+                LocalDate.of(2000, 1, 1),
+                "01012345678",
+                "12345",
+                "67890",
+                "123456",
+                ServiceCode.PROTECT_SERVICE);
+    }
+}
