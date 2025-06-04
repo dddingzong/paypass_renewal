d5479850ee4db0bfb6efee9e95bda47932101a50
USER <cji513@naver.com>
2025-06-04 17:32:41 +0900

    chore(UserRepositoryTest): 메서드 명 및 DisplayName 변경

--- src/test/java/com/project/paypass_renewal/repository/UserRepositoryTest.java
+++ src/test/java/com/project/paypass_renewal/repository/UserRepositoryTest.java
@@ -1,14 +1,12 @@
 package com.project.paypass_renewal.repository;
 
-import com.project.paypass_renewal.domain.ServiceCode;
 import com.project.paypass_renewal.domain.User;
+import com.project.paypass_renewal.support.UserUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDate;
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -17,12 +15,10 @@ class UserRepositoryTest {
     private UserRepository userRepository;
 
     @Test
-    @DisplayName("신규유저_저장_테스트")
-    void saveNewUser(){
+    @DisplayName("리포지토리_신규유저_저장_테스트")
+    void saveNewUserTest(){
         // given
-        User user = new User("test@gmail.com", "정종인",
-                LocalDate.of(2000, 5, 13), "01012345678",
-                "01675", "01747", "245863", ServiceCode.PROTECT_SERVICE);
+        User user = UserUtils.createDummyUser();
 
         // when
         userRepository.save(user);
