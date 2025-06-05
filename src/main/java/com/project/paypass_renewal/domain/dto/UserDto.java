8db91deaf4a3672e1e2211318f04794e54e0e887
USER <cji513@naver.com>
2025-06-05 16:39:35 +0900

    feat(UserDto): UserDto 구현

---
+++ src/main/java/com/project/paypass_renewal/domain/dto/UserDto.java
@@ -0,0 +1,36 @@
+package com.project.paypass_renewal.domain.dto;
+
+import com.project.paypass_renewal.domain.ServiceCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+public class UserDto {
+
+    private String mainId;
+
+    private String name;
+
+    private LocalDate birth;
+
+    private String number;
+
+    private String homeAddress;
+
+    private String centerAddress;
+
+    private ServiceCode serviceCode;
+
+    public UserDto(String mainId, String name, LocalDate birth, String number, String homeAddress, String centerAddress, ServiceCode serviceCode) {
+        this.mainId = mainId;
+        this.name = name;
+        this.birth = birth;
+        this.number = number;
+        this.homeAddress = homeAddress;
+        this.centerAddress = centerAddress;
+        this.serviceCode = serviceCode;
+    }
+}
