b4bdff553aa5a63099df19a37872a693a4c84925
USER <cji513@naver.com>
2025-06-05 16:37:58 +0900

    test(UserServiceTest): UserService TDD 작성

--- src/test/java/com/project/paypass_renewal/service/UserServiceTest.java
+++ src/test/java/com/project/paypass_renewal/service/UserServiceTest.java
@@ -1,9 +1,10 @@
 package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.User;
+import com.project.paypass_renewal.domain.dto.UserDto;
+import com.project.paypass_renewal.generator.LinkCodeGenerator;
 import com.project.paypass_renewal.repository.UserRepository;
-import com.project.paypass_renewal.support.UserUtils;
-import org.assertj.core.api.Assertions;
+import com.project.paypass_renewal.support.UserDtoTestUtil;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,23 +21,52 @@ class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
+    @Mock
+    LinkCodeGenerator linkCodeGenerator;
+
     @InjectMocks
     UserService userService;
 
     @Test
     @DisplayName("서비스_신규유저_저장_테스트")
-    void saveNewUser() {
+    void saveNewUserTest() {
         // given
-        User user = UserUtils.createDummyUser();
-
-        // Mocking the repository save method
-        when(userRepository.save(user)).thenReturn(user);
+        UserDto userDto = UserDtoTestUtil.createDummyUserDto();
+        String linkCode = "ABC123";
+        // stub
+        when(linkCodeGenerator.generate()).thenReturn(linkCode);
+        when(userRepository.existsByLinkCode(linkCode)).thenReturn(false);
 
         // when
-        User savedUser = userService.save(user);
+        User user = userService.saveNewUser(userDto);
 
         // then
-        Assertions.assertThat(savedUser.getId()).isEqualTo(user.getId());
+        assertThat(user.getMainId()).isNotNull();
+        assertThat(user.getMainId()).isEqualTo(userDto.getMainId());
+        assertThat(user.getLinkCode()).isNotNull();
+        assertThat(user.getLinkCode()).isEqualTo(linkCode);
+    }
+
+    @Test
+    @DisplayName("서비스_신규유저_링크코드_중복_테스트")
+    void linkCodeDuplicateTest(){
+        // given
+        UserDto userDto = UserDtoTestUtil.createDummyUserDto();
+        String firstLinkCode = "123ABC";
+        String secondLinkCode = "456DEF";
+        // stub
+        when(linkCodeGenerator.generate()).thenReturn(firstLinkCode,secondLinkCode);
+        when(userRepository.existsByLinkCode(firstLinkCode)).thenReturn(true);
+        when(userRepository.existsByLinkCode(secondLinkCode)).thenReturn(false);
+
+        // when
+        User user = userService.saveNewUser(userDto);
+
+        // then
+        assertThat(user.getMainId()).isNotNull();
+        assertThat(user.getMainId()).isEqualTo(userDto.getMainId());
+        assertThat(user.getLinkCode()).isNotNull();
+        assertThat(user.getLinkCode()).isEqualTo(secondLinkCode);
     }
 
 }
