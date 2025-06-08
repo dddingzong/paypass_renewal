cdefacccdf3164503b103f231494a3e7b7c6f636
USER <cji513@naver.com>
2025-06-08 19:28:22 +0900

    feat(UserDto): 유효성 검사 어노테이션 추가

--- src/main/java/com/project/paypass_renewal/domain/dto/UserDto.java
+++ src/main/java/com/project/paypass_renewal/domain/dto/UserDto.java
@@ -2,7 +2,7 @@ package com.project.paypass_renewal.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.paypass_renewal.domain.ServiceCode;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,11 +13,19 @@ import java.time.LocalDate;
 public class UserDto {
 
     @NotNull(message = "이메일은 필수 값입니다.")
+    @NotEmpty(message = "이메일은 빈칸으로 작성할 수 없습니다")
+    @Email(message = "이메일 형식을 지켜주십시오")
     private String mainId;
 
+    @NotNull(message = "이름은 필수 값입니다.")
+    @NotEmpty(message = "이름은 빈칸으로 작성할 수 없습니다")
+    @Pattern(regexp = "^[가-힣]{2,4}$", message = "이름은 한글로 2~4자 사이로 작성해주세요")
     private String name;
 
+    // LocalDate 타입은 @Valid 사용 불가, 따로 예외처리 진행
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Past(message = "생년월일은 과거 날짜여야 합니다")
+    @NotNull(message = "생년월일은 필수 값입니다.")
     private LocalDate birth;
 
     private String number;
