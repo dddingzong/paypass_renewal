327086d9ddd2f8f982886f049fbf3a33000de72b
USER <cji513@naver.com>
2025-06-11 18:48:45 +0900

    feat(LinkRequestDto): LinkRequestDto 생성

---
+++ src/main/java/com/project/paypass_renewal/domain/dto/LinkRequestDto.java
@@ -0,0 +1,16 @@
+package com.project.paypass_renewal.domain.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LinkRequestDto {
+    private String supporterNumber;
+    private String userNumber;
+
+    public LinkRequestDto(String supporterNumber, String userNumber) {
+        this.supporterNumber = supporterNumber;
+        this.userNumber = userNumber;
+    }
+}
