1958df65c28d558b5a6e06acefd592dbb1ee1750
USER <cji513@naver.com>
2025-06-14 15:25:52 +0900

    feat(LinkController): getLinkList 구현

--- src/main/java/com/project/paypass_renewal/controller/LinkController.java
+++ src/main/java/com/project/paypass_renewal/controller/LinkController.java
@@ -1,12 +1,17 @@
 package com.project.paypass_renewal.controller;
 
-import com.project.paypass_renewal.domain.dto.LinkRequestDto;
+import com.project.paypass_renewal.domain.User;
+import com.project.paypass_renewal.domain.dto.request.LinkRequestDto;
+import com.project.paypass_renewal.domain.dto.request.SupporterNumberRequestDto;
+import com.project.paypass_renewal.domain.dto.response.LinkListResponseDto;
 import com.project.paypass_renewal.service.LinkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +23,17 @@ public class LinkController {
     public ResponseEntity<String> saveNewLink(@RequestBody LinkRequestDto linkRequestDto) {
         linkService.saveNewLink(linkRequestDto);
         return ResponseEntity.ok("saveSuccess");
+    }
+
+    @PostMapping("/link/getLinkList")
+    public ResponseEntity<List<LinkListResponseDto>> getLinkList(@RequestBody SupporterNumberRequestDto supporterNumberRequestDto) {
+        List<String> userNumbers = linkService.findUserNumbersBySupporterNumber(supporterNumberRequestDto);
+
+        List<User> userList = linkService.findUserListByNumbers(userNumbers);
+
+        List<LinkListResponseDto> linkListResponseDtoList = linkService.userToLinkResponseDto(userList);
+
+        return ResponseEntity.ok(linkListResponseDtoList);
     }
 
 }
