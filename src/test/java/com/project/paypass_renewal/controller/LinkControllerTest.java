5948c21857de0f46854b3dbac66a3987fc0693ec
USER <cji513@naver.com>
2025-06-14 16:37:25 +0900

    test(LinkController): 링크 중복 저장 TDD 작성

--- src/test/java/com/project/paypass_renewal/controller/LinkControllerTest.java
+++ src/test/java/com/project/paypass_renewal/controller/LinkControllerTest.java
@@ -6,6 +6,7 @@ import com.project.paypass_renewal.domain.User;
 import com.project.paypass_renewal.domain.dto.request.LinkRequestDto;
 import com.project.paypass_renewal.domain.dto.request.SupporterNumberRequestDto;
 import com.project.paypass_renewal.domain.dto.response.LinkListResponseDto;
+import com.project.paypass_renewal.exception.handler.GlobalExceptionHandler;
 import com.project.paypass_renewal.service.LinkService;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -43,8 +44,11 @@ class LinkControllerTest {
 
     @BeforeEach
     void init() {
-        mockMvc = MockMvcBuilders.standaloneSetup(linkController).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(linkController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
         objectMapper = new ObjectMapper();
+
     }
 
     @Test
@@ -65,6 +69,29 @@ class LinkControllerTest {
         // then
         result.andExpect(status().isOk())
                 .andExpect(content().string("saveSuccess"));
+    }
+
+    @Test
+    @DisplayName("링크_중복_저장_테스트")
+    void checkDuplicateLinkTest() throws Exception {
+        // given
+        final String url = "/link/saveNewLink";
+        LinkRequestDto linkDto = new LinkRequestDto("01089091234", "01012341234");
+        String json = objectMapper.writeValueAsString(linkDto);
+
+        // stub
+        when(linkService.checkDuplicateLink(any(LinkRequestDto.class))).thenReturn(true);
+
+        // when
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.post(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+        );
+
+        // then
+        result.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").exists());
     }
 
     // 보내야하는 정보: 이름, 주소
