31a00d6dd5b416053735aaeac4aac4f39a558406
USER <cji513@naver.com>
2025-06-14 16:37:59 +0900

    feat(ErrorResult): CustomException 추가

--- src/main/java/com/project/paypass_renewal/exception/ErrorResult.java
+++ src/main/java/com/project/paypass_renewal/exception/ErrorResult.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorResult {
 
-    USER_NUMBER_DUPLICATE(HttpStatus.BAD_REQUEST, "중복된 전화번호입니다.");
+    USER_NUMBER_DUPLICATE(HttpStatus.BAD_REQUEST, "중복된 전화번호입니다."),
+    LINK_USER_AND_SUPPORTER_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 등록된 이용자와 보호자입니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
