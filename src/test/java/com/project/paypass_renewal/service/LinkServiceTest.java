4dc386ce426ac18ff1769c8132b547772230a253
USER <cji513@naver.com>
2025-06-14 15:23:27 +0900

    test(LinkServiceTest): UserToLinkResponseDto TDD 작성

--- src/test/java/com/project/paypass_renewal/service/LinkServiceTest.java
+++ src/test/java/com/project/paypass_renewal/service/LinkServiceTest.java
@@ -1,15 +1,20 @@
 package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.Link;
-import com.project.paypass_renewal.domain.dto.LinkRequestDto;
+import com.project.paypass_renewal.domain.ServiceCode;
+import com.project.paypass_renewal.domain.User;
+import com.project.paypass_renewal.domain.dto.response.LinkListResponseDto;
+import com.project.paypass_renewal.domain.dto.request.LinkRequestDto;
 import com.project.paypass_renewal.repository.LinkRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -37,6 +42,24 @@ class LinkServiceTest {
         assertThat(link.getUserNumber()).isEqualTo("01012341234");
     }
 
+    @Test
+    @DisplayName("사용자_링크_리스트_조회_테스트")
+    void UserToLinkResponseDto () {
+        // List<User> -> List<LinkResponseDto>로 변환
+        // given
+        List<User> userList = List.of(
+                new User("test1", "abc123", LocalDate.of(2000, 01, 01), "01011111111", "12352", null, "AG1DE1", ServiceCode.CARE_SERVICE),
+                new User("test2", "abc123", LocalDate.of(2000, 01, 02), "01022222222", "12152", null, "AG1DEV", ServiceCode.CARE_SERVICE)
+        );
+
+        // when
+        List<LinkListResponseDto> LinkList = linkService.userToLinkResponseDto(userList);
+
+        // then
+        assertThat(LinkList).hasSize(2);
+        assertThat(LinkList.get(0).getUserNumber()).isEqualTo("01011111111");
+        assertThat(LinkList.get(1).getUserNumber()).isEqualTo("01022222222");
+    }
 
 
 }
