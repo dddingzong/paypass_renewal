8a8ac63eeadc2fa0a1662650c4cdad4ef83eda37
USER <cji513@naver.com>
2025-06-14 15:25:29 +0900

    feat(LinkService): LinkList 생성에 필요한 메서드 구현

--- src/main/java/com/project/paypass_renewal/service/LinkService.java
+++ src/main/java/com/project/paypass_renewal/service/LinkService.java
@@ -1,16 +1,24 @@
 package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.Link;
-import com.project.paypass_renewal.domain.dto.LinkRequestDto;
+import com.project.paypass_renewal.domain.User;
+import com.project.paypass_renewal.domain.dto.request.SupporterNumberRequestDto;
+import com.project.paypass_renewal.domain.dto.response.LinkListResponseDto;
+import com.project.paypass_renewal.domain.dto.request.LinkRequestDto;
 import com.project.paypass_renewal.repository.LinkRepository;
+import com.project.paypass_renewal.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class LinkService {
 
     private final LinkRepository linkRepository;
+    private final UserRepository userRepository;
+
 
     public Link saveNewLink(LinkRequestDto linkRequestDto) {
 
@@ -19,6 +27,28 @@ public class LinkService {
         linkRepository.save(link);
 
         return link;
+    }
+
+    public List<String> findUserNumbersBySupporterNumber(SupporterNumberRequestDto supporterNumberRequestDto) {
+        String supporterNumber = supporterNumberRequestDtoToString(supporterNumberRequestDto);
+        return linkRepository.findUserNumbersBySupporterNumber(supporterNumber);
+    }
+
+    public List<User> findUserListByNumbers (List<String> userNumbers) {
+        return userRepository.findByNumberIn(userNumbers);
+    }
+
+    public List<LinkListResponseDto> userToLinkResponseDto(List<User> userList) {
+        return userList.stream()
+                .map(user -> new LinkListResponseDto(
+                        user.getNumber(),
+                        user.getName(),
+                        user.getHomeAddress()))
+                .toList();
+    }
+
+    private String supporterNumberRequestDtoToString(SupporterNumberRequestDto supporterNumberRequestDto) {
+        return supporterNumberRequestDto.getSupporterNumber();
     }
 
     private Link toEntity(LinkRequestDto linkRequestDto) {
