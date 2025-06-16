97eabf894ac69755b838ffb28eebe48682afd5e2
USER <cji513@naver.com>
2025-06-16 19:34:21 +0900

    test(UpdateMyPageTest): 사용자 센터 주소 변경 TDD 작성

--- src/test/java/com/project/paypass_renewal/service/UpdateMyPageTest.java
+++ src/test/java/com/project/paypass_renewal/service/UpdateMyPageTest.java
@@ -2,6 +2,7 @@ package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.User;
 import com.project.paypass_renewal.domain.UserAddress;
+import com.project.paypass_renewal.domain.dto.request.CenterAddressRequestDto;
 import com.project.paypass_renewal.domain.dto.request.HomeAddressRequestDto;
 import com.project.paypass_renewal.repository.UserAddressRepository;
 import com.project.paypass_renewal.repository.UserRepository;
@@ -32,7 +33,6 @@ class UpdateMyPageTest {
     void updateHomeAddressTest() {
         // 변경사항: homeAddress, homeStreetAddress, homeStreetAddressDetail
         // given
-
         String number = "01012345678";
 
         HomeAddressRequestDto homeAddressRequestDto = new HomeAddressRequestDto(number,"01849","서울특별시 노원구 공릉로 95","102");
@@ -52,7 +52,26 @@ class UpdateMyPageTest {
         assertThat(userAddressRepository.findByNumber(number).getHomeStreetAddressDetail()).isEqualTo(homeAddressRequestDto.getHomeStreetAddressDetail());
     }
 
+    @Test
+    @DisplayName("유저_센터주소_변경_테스트")
+    void updateCenterAddressTest() {
+        // 변경사항: centerAddress, centerStreetAddress
+        // given
+        String number = "01012345678";
 
+        CenterAddressRequestDto centerAddressRequestDto = new CenterAddressRequestDto(number, "12315", "서울시 변경 후 테스트 데이터");
 
+        User user = UserTestUtils.createDummyUser();
+        UserAddress userAddress = new UserAddress(number, "서울시 노원구 노원로 564", "1001-102", "서울 노원구 노원로18길 41");
 
+        userRepository.save(user);
+        userAddressRepository.save(userAddress);
+
+        // when
+        myPageService.updateCenterAddress(centerAddressRequestDto);
+
+        // then
+        assertThat(userRepository.findByNumber(number).getCenterAddress()).isEqualTo(centerAddressRequestDto.getCenterAddress());
+        assertThat(userAddressRepository.findByNumber(number).getCenterStreetAddress()).isEqualTo(centerAddressRequestDto.getCenterStreetAddress());
+    }
 }
