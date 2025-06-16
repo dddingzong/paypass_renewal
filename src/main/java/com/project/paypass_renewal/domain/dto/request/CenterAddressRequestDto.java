81841ae5e3003a97e2f76ea80b916b11b98f25df
USER <cji513@naver.com>
2025-06-16 19:47:53 +0900

    chore(CenterAddressRequestDto): dto 유효성 검사 추가

--- src/main/java/com/project/paypass_renewal/domain/dto/request/CenterAddressRequestDto.java
+++ src/main/java/com/project/paypass_renewal/domain/dto/request/CenterAddressRequestDto.java
@@ -1,5 +1,6 @@
 package com.project.paypass_renewal.domain.dto.request;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CenterAddressRequestDto {
     private String number;
+
+    @Pattern(regexp = "^\\d{5}$", message = "우편번호는 5자리 숫자여야 합니다. 거주 센터가 없으시다면 빈칸으로 제출해 주십시오")
     private String centerAddress;
+
     private String centerStreetAddress;
 
     public CenterAddressRequestDto(String number, String centerAddress, String centerStreetAddress) {
