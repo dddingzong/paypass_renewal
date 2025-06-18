b69f453131dd256e50b9929d7e69137d19064efc
USER <cji513@naver.com>
2025-06-18 20:47:54 +0900

    feat(ErrorResult): 비밀번호 매칭, 전화번호 존재 유무 예외 생성

--- src/main/java/com/project/paypass_renewal/exception/ErrorResult.java
+++ src/main/java/com/project/paypass_renewal/exception/ErrorResult.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorResult {
 
     USER_NUMBER_DUPLICATE(HttpStatus.BAD_REQUEST, "중복된 전화번호입니다."),
-    LINK_USER_AND_SUPPORTER_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 등록된 이용자와 보호자입니다.");
+    LINK_USER_AND_SUPPORTER_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 등록된 이용자와 보호자입니다."),
+    NOT_EXIST_NUMBER(HttpStatus.BAD_REQUEST, "존재하지 않는 전화번호 입니다."),
+    USER_NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
