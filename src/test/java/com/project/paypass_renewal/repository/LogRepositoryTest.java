6550ca838224ff21c46c89aa5268ba0b94d56ccd
USER <cji513@naver.com>
2025-06-20 15:23:57 +0900

    test(all): properties 설정 추가

--- src/test/java/com/project/paypass_renewal/repository/LogRepositoryTest.java
+++ src/test/java/com/project/paypass_renewal/repository/LogRepositoryTest.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class LogRepositoryTest {
