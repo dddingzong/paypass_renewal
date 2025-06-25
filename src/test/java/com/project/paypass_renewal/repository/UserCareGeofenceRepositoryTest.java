777048a17490de7a01ee5c2922480f45ca7b7460
USER <cji513@naver.com>
2025-06-25 13:45:47 +0900

    chore(UserCareGeofenceRepositoryTest): @ActiveProfiles("test") 어노테이션 추가

--- src/test/java/com/project/paypass_renewal/repository/UserCareGeofenceRepositoryTest.java
+++ src/test/java/com/project/paypass_renewal/repository/UserCareGeofenceRepositoryTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class UserCareGeofenceRepositoryTest {
