92c1fd4b0bdbee0505601d7fbb66e9d38fbe80da
USER <cji513@naver.com>
2025-07-04 22:47:39 +0900

    test(UserLocationRepositoryTest): UserLocationRepositoryTest findByNumber TDD 작성

--- src/test/java/com/project/paypass_renewal/repository/UserLocationRepositoryTest.java
+++ src/test/java/com/project/paypass_renewal/repository/UserLocationRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -22,7 +23,7 @@ class UserLocationRepositoryTest {
 
     @Test
     @DisplayName("실시간_위치_저장")
-    void saveUserLocationTest(){
+    void saveUserLocationTest() {
         // given
         UserLocation userLocation = new UserLocation("01012345678", BigDecimal.valueOf(37.6616521), BigDecimal.valueOf(127.0561246));
 
@@ -33,5 +34,27 @@ class UserLocationRepositoryTest {
         Long id = savedUserLocation.getId();
 
         assertThat(id).isEqualTo(userLocation.getId());
+    }
+
+    @Test
+    @DisplayName("유저_위치_리스트_조회")
+    void findByNumberOrderBySavedTimeDescTest() {
+        // given
+        String number = "01012345678";
+
+        UserLocation userLocationOne = new UserLocation(number, BigDecimal.valueOf(37.6616521), BigDecimal.valueOf(127.0561246));
+        UserLocation userLocationTwo = new UserLocation(number, BigDecimal.valueOf(37.6616523), BigDecimal.valueOf(127.0561213));
+        UserLocation userLocationThree = new UserLocation("01011112222", BigDecimal.valueOf(37.6616121), BigDecimal.valueOf(127.0561463));
+
+        userLocationRepository.save(userLocationOne);
+        userLocationRepository.save(userLocationTwo);
+        userLocationRepository.save(userLocationThree);
+        // when
+        List<UserLocation> userLocations = userLocationRepository.findByNumberOrderBySavedTimeDesc(number);
+
+        // then
+        assertThat(userLocations).hasSize(2);
+        assertThat(userLocations.get(0).getNumber()).isEqualTo(number);
+        assertThat(userLocations.get(1).getNumber()).isEqualTo(number);
     }
 }
