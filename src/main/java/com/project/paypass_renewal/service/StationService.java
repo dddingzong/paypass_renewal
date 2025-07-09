c2f8f8304c17834c933959cdd7a538af064fa8a6
USER <cji513@naver.com>
2025-07-09 17:27:25 +0900

    feat(StationService): getStationList 메서드 생성

--- src/main/java/com/project/paypass_renewal/service/StationService.java
+++ src/main/java/com/project/paypass_renewal/service/StationService.java
@@ -1,6 +1,7 @@
 package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.data.Station;
+import com.project.paypass_renewal.domain.dto.response.StationListResponseDto;
 import com.project.paypass_renewal.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,18 @@ public class StationService {
 
     private final StationRepository stationRepository;
 
-    public List<Station> findAll() {
-       return stationRepository.findAll();
+    public List<StationListResponseDto> getStationList() {
+        List<Station> stationList = stationRepository.findAll();
+
+        return stationList.stream().map(
+                station -> new StationListResponseDto(
+                        station.getName(),
+                        station.getStationNumber(),
+                        station.getLongitude(),
+                        station.getLatitude()
+                )
+        ).toList();
+
     }
 
 }
