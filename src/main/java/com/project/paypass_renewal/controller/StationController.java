9067c23170150ad24be23a191dc48072a94b54fe
USER <cji513@naver.com>
2025-07-09 16:55:07 +0900

    chore(StationController): get mapping으로 변경

--- src/main/java/com/project/paypass_renewal/controller/StationController.java
+++ src/main/java/com/project/paypass_renewal/controller/StationController.java
@@ -4,7 +4,7 @@ import com.project.paypass_renewal.domain.data.Station;
 import com.project.paypass_renewal.service.StationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,7 +15,7 @@ public class StationController {
 
     private final StationService stationService;
 
-    @PostMapping("/station/getStationList")
+    @GetMapping("/station/getStationList")
     public ResponseEntity<List<Station>> getStationList() {
         List<Station> stationList = stationService.findAll();
         return ResponseEntity.ok(stationList);
