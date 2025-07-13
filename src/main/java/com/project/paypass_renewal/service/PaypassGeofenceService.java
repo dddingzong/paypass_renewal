20ec2963b4e60e0d119a5c9d8991929d791368f2
USER <cji513@naver.com>
2025-07-13 22:29:23 +0900

    feat(PaypassGeofenceService): 정류장 이탈 시 service 단 작동 로직 구현

--- src/main/java/com/project/paypass_renewal/service/PaypassGeofenceService.java
+++ src/main/java/com/project/paypass_renewal/service/PaypassGeofenceService.java
@@ -2,8 +2,12 @@ package com.project.paypass_renewal.service;
 
 import com.project.paypass_renewal.domain.PaypassGeofence;
 import com.project.paypass_renewal.repository.PayPassGeofenceRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,13 +15,38 @@ public class PaypassGeofenceService {
 
     private final PayPassGeofenceRepository payPassGeofenceRepository;
 
-
+    @Transactional
     public PaypassGeofence createGeofenceLocation(String number, Long stationNumber, String busInfo) {
         PaypassGeofence paypassGeofence = new PaypassGeofence(number, stationNumber, busInfo);
         payPassGeofenceRepository.save(paypassGeofence);
 
         return paypassGeofence;
+    }
+
+    public List<PaypassGeofence> findByNumberAndStationNumber(String number, Long stationNumber) {
+        return payPassGeofenceRepository.findByNumberAndStationNumber(number, stationNumber);
 
     }
+
+    public PaypassGeofence userFenceOutWithoutEntity(String number, Long stationNumber, String busInfo, LocalDateTime userFenceOut) {
+        return new PaypassGeofence(number, stationNumber, busInfo, userFenceOut);
+    }
+
+    @Transactional
+    public PaypassGeofence save(PaypassGeofence paypassGeofence) {
+        return payPassGeofenceRepository.save(paypassGeofence);
+    }
+
+    @Transactional
+    public void userFenceOut(PaypassGeofence paypassGeofence) {
+        paypassGeofence.userFenceOut();
+    }
+
+    @Transactional
+    public boolean fenceOutTimeIsNull(PaypassGeofence paypassGeofence){
+        return paypassGeofence.fenceOutTimeIsNull();
+    }
+
+
 
 }
