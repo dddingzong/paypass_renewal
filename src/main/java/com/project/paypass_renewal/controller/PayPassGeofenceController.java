d8d49b6137d82b66a844251f1edc6535736058d5
USER <cji513@naver.com>
2025-07-13 22:29:39 +0900

    feat(PayPassGeofenceController): 정류장 이탈 시 기능 구현

--- src/main/java/com/project/paypass_renewal/controller/PayPassGeofenceController.java
+++ src/main/java/com/project/paypass_renewal/controller/PayPassGeofenceController.java
@@ -1,5 +1,6 @@
 package com.project.paypass_renewal.controller;
 
+import com.project.paypass_renewal.domain.PaypassGeofence;
 import com.project.paypass_renewal.domain.dto.request.UserPaypassGeofenceRequestDto;
 import com.project.paypass_renewal.service.PaypassGeofenceService;
 import com.project.paypass_renewal.service.StationService;
@@ -9,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Slf4j
@@ -37,21 +41,64 @@ public class PayPassGeofenceController {
         return ResponseEntity.ok("success save geofence data");
     }
 
-//    @PostMapping("/geofence/userFenceOut")
-//    public ResponseEntity<PaypassGeofence> userFenceOut(@RequestBody UserPaypassGeofenceRequestDto userPaypassGeofenceRequestDto) {
-//        log.info("****************************************************************");
-//        log.info("사용자가 geofence에서 이탈했기 때문에 userGeofenceOut method를 실행합니다.");
-//        log.info("****************************************************************");
-//
-//        String number = userPaypassGeofenceRequestDto.getNumber();
-//        Long stationNumber = userPaypassGeofenceRequestDto.getStationNumber();
-//
-//        // mainId와 stationNumber를 활용해서 해당 entity 가져오기
-//        List<PaypassGeofence> paypassGeofences = paypassGeofenceService.findByMainIdAndStationNumber(mainId, stationNumber);
-//
-//
-//        return null;
-//    }
+    @PostMapping("/geofence/userFenceOut")
+    public ResponseEntity<String> userFenceOut(@RequestBody UserPaypassGeofenceRequestDto userPaypassGeofenceRequestDto) {
+        log.info("****************************************************************");
+        log.info("사용자가 geofence에서 이탈했기 때문에 userGeofenceOut method를 실행합니다.");
+        log.info("****************************************************************");
+
+        String number = userPaypassGeofenceRequestDto.getNumber();
+        Long stationNumber = userPaypassGeofenceRequestDto.getStationNumber();
+
+        // number와 stationNumber를 활용해서 해당 entity 가져오기
+        List<PaypassGeofence> paypassGeofences = paypassGeofenceService.findByNumberAndStationNumber(number, stationNumber);
+
+        // geofenceList의 조건에 따라서 fenceOutTime 업데이트
+        // entity가 존재하지 않으면 2분 전 fenceIn을 한다고 가정한다.
+        if (paypassGeofences.isEmpty()){
+            updateFenceOutTimeNoEntity(number, stationNumber);
+        }
+
+        // entity가 하나라면 해당 entity의 fenceOutTime 추가
+        if (paypassGeofences.size() == 1){
+            updateFenceOutTimeOneEntity(paypassGeofences);
+        }
+
+        // 조회하였을 때 두개 이상인 경우에는 userFenceOut이 null 값인 엔티티 찾기
+        if (paypassGeofences.size() > 1){
+            updateFenceOutTimeEntities(paypassGeofences);
+        }
+
+        return ResponseEntity.ok("success update fenceOutTime");
+    }
+
+    private void updateFenceOutTimeNoEntity(String number, Long stationNumber){
+        log.info("stationNumber에 부합하는 entity가 존재하지 않기 때문에 2분 전 fenceIn을 가정하고 entity를 생성합니다.");
+
+        // stationNumber 활용해서 busInfo 가져오기
+        String busInfo = stationService.findBusInfoByStationNumber(stationNumber);
+
+        // paypassGeofence entity 생성
+        PaypassGeofence paypassGeofence = paypassGeofenceService.userFenceOutWithoutEntity(number, stationNumber, busInfo, LocalDateTime.now());
+
+        paypassGeofenceService.save(paypassGeofence);
+        log.info("paypassGeofence 데이터를 생성 후 저장했습니다. (entity 없이 fenceOut한 경우)");
+        log.info("paypassGeofence: {}", paypassGeofence);
+    }
+
+    private void updateFenceOutTimeOneEntity(List<PaypassGeofence> paypassGeofences){
+        PaypassGeofence paypassGeofence = paypassGeofences.get(0);
+        paypassGeofenceService.save(paypassGeofence);
+        log.info("하나의 entity를 발견하여 fenceOutTime을 추가합니다.");
+    }
+
+    private void updateFenceOutTimeEntities(List<PaypassGeofence> paypassGeofences) {
+        paypassGeofences.stream()
+                .filter(paypassGeofenceService::fenceOutTimeIsNull)
+                .findFirst()
+                .ifPresent(paypassGeofenceService::userFenceOut);
+        log.info("여러 개의 entity 중 첫 번째 null fenceOutTime을 업데이트했습니다.");
+    }
 
 
 }
