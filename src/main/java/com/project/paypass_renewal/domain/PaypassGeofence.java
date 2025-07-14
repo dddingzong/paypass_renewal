12963b3898f267add3bbf5a05dcfd09eabac6d14
USER <cji513@naver.com>
2025-07-14 19:27:30 +0900

    chore(PaypassGeofence): toString 수정

--- src/main/java/com/project/paypass_renewal/domain/PaypassGeofence.java
+++ src/main/java/com/project/paypass_renewal/domain/PaypassGeofence.java
@@ -3,13 +3,11 @@ package com.project.paypass_renewal.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@ToString
 @NoArgsConstructor
 public class PaypassGeofence {
     @Id
