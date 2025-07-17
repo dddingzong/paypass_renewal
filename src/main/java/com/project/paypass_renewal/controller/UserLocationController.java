8750892e9b1ddb9ae0fba668e735865bb4a469d1
USER <cji513@naver.com>
2025-07-17 21:07:58 +0900

    feat(UserLocationController): 유저 이동 경로 조회 기능 구현

--- src/main/java/com/project/paypass_renewal/controller/UserLocationController.java
+++ src/main/java/com/project/paypass_renewal/controller/UserLocationController.java
@@ -1,16 +1,23 @@
 package com.project.paypass_renewal.controller;
 
 
+import com.project.paypass_renewal.domain.dto.request.DetailLogPathRequestDto;
 import com.project.paypass_renewal.domain.dto.request.NumberRequestDto;
 import com.project.paypass_renewal.domain.dto.request.UserLocationRequestDto;
 import com.project.paypass_renewal.domain.dto.response.UserLocationResponseDto;
 import com.project.paypass_renewal.service.UserLocationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserLocationController {
@@ -32,6 +39,15 @@ public class UserLocationController {
         return ResponseEntity.ok(userLocationResponseDto);
     }
 
+    @PostMapping("/user/getUserLocationList")
+    public ResponseEntity<List<Map<String, BigDecimal>>> getUserMovingPath(@RequestBody DetailLogPathRequestDto detailLogPathRequestDto) {
+        log.info("사용자 세부로그 이동 경로를 조회합니다");
+        
+        List<Map<String, BigDecimal>> latLngList = userLocationService.getUserMovingPath(detailLogPathRequestDto);
+
+        System.out.println("latLngList = " + latLngList);
+        return ResponseEntity.ok(latLngList);
+    }
 
 
 
