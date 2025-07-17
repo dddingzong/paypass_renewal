5accc9cdda4e8ee7aaa89be4a361c0037be43d30
USER <cji513@naver.com>
2025-07-17 11:17:20 +0900

    feat(PaypassGeofenceService): 페이패스 알고리즘 작동 트리거 구현
    - LogService: 알고리즘 작동 후 로그 데이터 저장 로직 구현
    - DetailLogService: 알고리즘 작동 후 세부 로그 데이터 저장 로직 구현

--- src/main/java/com/project/paypass_renewal/service/PaypassGeofenceService.java
+++ src/main/java/com/project/paypass_renewal/service/PaypassGeofenceService.java
@@ -7,20 +7,60 @@ import com.project.paypass_renewal.util.algorithm.PaypassDeleteDuplicateAlgorith
 import com.project.paypass_renewal.util.algorithm.PaypassSequenceAlgorithm;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PaypassGeofenceService {
 
     private final PayPassGeofenceRepository payPassGeofenceRepository;
+
+    private final LogService logService;
+
     private final PaypassSequenceAlgorithm paypassSequenceAlgorithm;
     private final PaypassAverageTimeAlgorithm paypassAverageTimeAlgorithm;
     private final PaypassDeleteDuplicateAlgorithm paypassDeleteDuplicateAlgorithm;
+
+
+    @Transactional
+    @Scheduled(fixedRate = 60000) // 1분마다 실행 (단위: ms)
+    public void algorithmTrigger(){
+        log.info("algorithmTrigger가 작동합니다.");
+        // 모든 유저에 대한 가장 최근 데이터를 가져오기
+        List<PaypassGeofence> recentGeofenceLocations = payPassGeofenceRepository.findMostRecentGeofenceLocationByFenceInTime();
+        // fenceInTime이 현재 시간과 30분 차이나는지 확인
+        LocalDateTime standardTime = LocalDateTime.now().minusMinutes(30);
+        log.info("현재 시간에 30분을 차감하겠습니다. 차감 시간: " + standardTime);
+
+        for (PaypassGeofence geofenceLocation : recentGeofenceLocations) {
+            LocalDateTime userFenceInTime = geofenceLocation.getFenceInTime();
+            // 30분 차이나면 메인 로직 실행
+            if (standardTime.isAfter(userFenceInTime)){
+                String number = geofenceLocation.getNumber();
+                startUserAlgorithm(number);
+            }
+        }
+    }
+
+    @Transactional
+    public void startUserAlgorithm(String number){
+        log.info("메인 알고리즘을 실행합니다. number = " + number);
+        Map<List<PaypassGeofence>, List<String>> resultMap = startPaypassAlgorithm(number);
+
+        // 이후 데이터 저장
+        logService.saveLogData(number, resultMap);
+
+        // geofenceLocation 데이터 삭제
+        deleteGeofenceLocations(number);
+        log.info(number + "의 메인 알고리즘 실행 성공");
+    }
 
     @Transactional
     private Map<List<PaypassGeofence>, List<String>> startPaypassAlgorithm(String number) {
@@ -32,6 +72,12 @@ public class PaypassGeofenceService {
         Map<List<PaypassGeofence>, List<String>> deleteDuplicateGeofenceMap = paypassDeleteDuplicateAlgorithm.algorithmStart(averageTimeGeofenceMap, geofenceList);
 
         return deleteDuplicateGeofenceMap;
+    }
+
+    @Transactional
+    private void deleteGeofenceLocations(String number){
+        payPassGeofenceRepository.deleteByNumber(number);
+        log.info(number + "의 geofenceLocation 데이터 삭제 성공");
     }
 
     @Transactional
